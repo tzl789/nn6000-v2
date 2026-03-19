d60959d85ba284b30f407f69dbfc17c0e7ba517a
tzl789 <kegu2111@gmail.com>
2026-03-19 16:55:56 +0800

    Comment out chmod command for athena_led
    Comment out the chmod command for athena_led scripts.

--- scripts/Roc-script.sh
+++ scripts/Roc-script.sh
@@ -64,7 +64,7 @@ git clone --depth=1 https://github.com/gdy666/luci-app-lucky package/luci-app-lu
 # git clone --depth=1 https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
 # git clone --depth=1 https://github.com/laipeng668/luci-app-gecoosac package/luci-app-gecoosac
 # git clone --depth=1 https://github.com/NONGFAH/luci-app-athena-led package/luci-app-athena-led
-chmod +x package/luci-app-athena-led/root/etc/init.d/athena_led package/luci-app-athena-led/root/usr/sbin/athena-led
+# chmod +x package/luci-app-athena-led/root/etc/init.d/athena_led package/luci-app-athena-led/root/usr/sbin/athena-led
 
 ### PassWall & OpenClash ###
 
